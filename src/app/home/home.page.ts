330b5305a5026acb6f092221d576e279fbc3f667
Makwale <makwale.em@gmail.com>
2021-02-02 05:51:02 -0800

    First commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { NotificationVisibility } from '@ionic-native/downloader/ngx';
+import { DownloadRequest } from '@ionic-native/downloader/ngx';
+import { Downloader } from '@ionic-native/downloader/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private downloader: Downloader) {}
+  
+  download(){
+    var request: DownloadRequest = {
+      uri: 'https://cdn.images.express.co.uk/img/dynamic/130/590x/modern-gentleman-qualities-traits-manners-respectful-kind-polite-841392.jpg',
+      title: 'MyDownload',
+      description: '',
+      mimeType: '',
+      visibleInDownloadsUi: true,
+      notificationVisibility: NotificationVisibility.VisibleNotifyCompleted,
+      destinationInExternalFilesDir: {
+          dirType: 'Downloads',
+          subPath: 'MyFile.apk'
+      }
+  };
+
+
+  this.downloader.download(request)
+          .then((location: string) => alert('File downloaded at: '+ location))
+          .catch((error: any) => alert(error.message));
+  }
 
 }
